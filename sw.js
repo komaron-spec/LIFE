0576b2392c337dfd75c24b82bcb36e1792610ac2
mao <komaron055@gmail.com>
2026-08-26 19:47:55 +0900

    Revert "style: add refracted glass world depth"
    This reverts commit da857f0611d633f4fe9c8e40737e1086cf8561e4.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "life-system-v40";
-const ASSETS = ["./", "./index.html", "./styles.css", "./core-overrides.css", "./app.js", "./atlas-data.js", "./skills-data.js", "./manifest.webmanifest", "./icons/icon.svg", "./assets/world-glass-depth.png"];
+const CACHE = "life-system-v39";
+const ASSETS = ["./", "./index.html", "./styles.css", "./core-overrides.css", "./app.js", "./atlas-data.js", "./skills-data.js", "./manifest.webmanifest", "./icons/icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", (event) => {
