cc72ec40549e4234c7a3b05f3a28c1189a768725
mao <komaron055@gmail.com>
2026-08-26 12:35:10 +0900

    feat: add archive atlas exploration

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "life-system-v10";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "life-system-v11";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./atlas-data.js", "./manifest.webmanifest", "./icons/icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", (event) => {
