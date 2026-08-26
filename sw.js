a66fda987528ae36099aac3819722ec4c0f0015c
mao <komaron055@gmail.com>
2026-08-26 19:55:47 +0900

    style: restore bright liquid glass surfaces

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "life-system-v39";
+const CACHE = "life-system-v41";
 const ASSETS = ["./", "./index.html", "./styles.css", "./core-overrides.css", "./app.js", "./atlas-data.js", "./skills-data.js", "./manifest.webmanifest", "./icons/icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
