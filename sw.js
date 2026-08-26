5d5218ab6a0efa95ea64a2cf05d70c7d94676dae
mao <komaron055@gmail.com>
2026-08-26 18:59:41 +0900

    feat: add ambient parallax to player core

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "life-system-v35";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./atlas-data.js", "./skills-data.js", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE = "life-system-v36";
+const ASSETS = ["./", "./index.html", "./styles.css", "./core-overrides.css", "./app.js", "./atlas-data.js", "./skills-data.js", "./manifest.webmanifest", "./icons/icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", (event) => {
